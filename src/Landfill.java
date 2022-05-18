bba60ff130d6f341fdb6363c75e88f80f7e98ddf
ecehavanci <ecehavanci@gmail.com>
2022-05-18 12:25:36 +0300

    Abstract factory for City in LocationContainer.java & Landfill.java has been implemented but hasnt implemented the entry point into application

--- src/Landfill.java
+++ src/Landfill.java
@@ -1,53 +1,169 @@
 
 //Abstract factory
-public class Landfill {
+abstract class Landfill {
+    abstract public RecyclableWaste collectRecyclableWaste();
 
+    abstract public NonRecyclableWaste collectNonRecyclableWaste();
 }
 
 //Concrete factory 1
 class MedicalLandfill extends Landfill {
 
+    @Override
+    public RecyclableWaste collectRecyclableWaste() {
+        return new RecyclableMedicalWaste("Li", "Liquid");
+    }
+
+    @Override
+    public NonRecyclableWaste collectNonRecyclableWaste() {
+        return new NonRecyclableMedicalWaste("Li", "Liquid");
+    }
 }
 
 //Concrete factory 2
 class GeneralLandfill extends Landfill {
 
+    @Override
+    public RecyclableWaste collectRecyclableWaste() {
+        return new RecyclableGeneralWaste("Fe", "Solid");
+    }
+
+    @Override
+    public NonRecyclableWaste collectNonRecyclableWaste() {
+        return new NonRecyclableGeneralWaste("Fe", "Solid");
+    }
 }
 
 //Abstract Product
-abstract class Waste{
+abstract class Waste {
+    abstract public String getWasteElement();   //waste element
+
+    abstract public String getWasteSubstance(); //if waste is solid,liquid or gas
+
+    abstract public boolean isRecyclable();
+
+    abstract public String getWasteType();
+
+    abstract public void printAllInfo();
 
 }
 
 //Abstract Product A
-abstract class RecyclableWaste extends Waste{
+//RecyclableWaste base class
+abstract class RecyclableWaste extends Waste {
+    protected String element;
+    protected String substance;
+    protected boolean isRecyclable;
+    protected String wasteType;
 
+    @Override
+    public String getWasteElement() {
+        return element;
+    }
+
+    @Override
+    public String getWasteSubstance() {
+        return substance;
+    }
+
+    @Override
+    public boolean isRecyclable() {
+        return true;
+    }
+
+    @Override
+    public String getWasteType() {
+        return wasteType;
+    }
+
+    @Override
+    public void printAllInfo() {
+        System.out.println("---- WASTE INFO ----");
+        System.out.println("Element: " + getWasteElement());
+        System.out.println("Substance: " + getWasteSubstance());
+        System.out.println("Recyclable: " + isRecyclable());
+        System.out.println("Waste Type: " + getWasteType());
+    }
 }
 
 //Concrete Product A1
-class RecyclableMedicalWaste extends RecyclableWaste{
+class RecyclableMedicalWaste extends RecyclableWaste {
+
+    public RecyclableMedicalWaste(String element, String substance) {
+        element = element;
+        substance = substance;
+        wasteType = "Medical Waste";
+    }
 
 }
 
 //Concrete Product A2
-class RecyclableGeneralWaste extends  RecyclableWaste{
+class RecyclableGeneralWaste extends RecyclableWaste {
+
+    public RecyclableGeneralWaste(String element, String substance) {
+        element = element;
+        substance = substance;
+        wasteType = "General Waste";
+    }
 
 }
 
 //Abstract Product B
-abstract class NonRecyclableWaste{
+//NonRecyclableWaste base class
+abstract class NonRecyclableWaste extends Waste {
+    protected String element;
+    protected String substance;
+    protected boolean isRecyclable;
+    protected String wasteType;
+
+    @Override
+    public String getWasteElement() {
+        return element;
+    }
+
+    @Override
+    public String getWasteSubstance() {
+        return substance;
+    }
+
+    @Override
+    public boolean isRecyclable() {
+        return false;
+    }
+
+    @Override
+    public String getWasteType() {
+        return wasteType;
+    }
+
+    @Override
+    public void printAllInfo() {
+        System.out.println("---- WASTE INFO ----");
+        System.out.println("Element: " + getWasteElement());
+        System.out.println("Substance: " + getWasteSubstance());
+        System.out.println("Recyclable: " + isRecyclable());
+        System.out.println("Waste Type: " + getWasteType());
+    }
 
 }
 
 //Concrete Product B1
-class NonRecyclableMedicalWaste extends NonRecyclableWaste{
+class NonRecyclableMedicalWaste extends NonRecyclableWaste {
 
+
+    public NonRecyclableMedicalWaste(String element, String substance) {
+        element = element;
+        substance = substance;
+        wasteType = "Mecical Waste";
+    }
 }
 
 //Concrete Product B2
-class NonRecyclableGeneralWaste extends  NonRecyclableWaste{
+class NonRecyclableGeneralWaste extends NonRecyclableWaste {
 
+    public NonRecyclableGeneralWaste(String element, String substance) {
+        element = element;
+        substance = substance;
+        wasteType = "General Waste";
+    }
 }
-
-
-
--- src/LocationContainer.java
+++ src/LocationContainer.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
 
 //Component
-interface LocatingElement{
+interface LocatingElement {
 
 }
 
 //Composite Object
-public class LocationContainer implements LocatingElement{
+public class LocationContainer implements LocatingElement {
 
 }
 
@@ -13,17 +14,31 @@ public class LocationContainer implements LocatingElement{
 
 
 //Cities are composite objects, they have neighborhoods in them
-class City extends LocationContainer{
+class City extends LocationContainer {
     //City has 3 landfills: one for medical waste, 2 for general use
     MedicalLandfill medicalLandfill;
     GeneralLandfill generalLandfill1;
     GeneralLandfill generalLandfill2;
 
+    //comes from abstract factory pattern, city is the client
+    //city uses Landfills
+    private ArrayList<Waste> wastes;
+
+    public void createLandfill(Landfill landfill) {
+        wastes = new ArrayList<>();
+        wastes.add(landfill.collectRecyclableWaste());
+        wastes.add(landfill.collectNonRecyclableWaste());
+    }
+
+    void displayLandfillParts() {
+        System.out.println("\tListing Wastes\n\t-------------");
+        wastes.forEach(waste -> waste.printAllInfo());
+    }
 }
 
 
 //Neighborhoods are composite objects, they have streets in them
-class Neighborhood extends LocationContainer{
+class Neighborhood extends LocationContainer {
 
 }
 
